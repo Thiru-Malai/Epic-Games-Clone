29c280fecfb69e5ae094623e54221af1bf5173d9
TharunDharmaraj <tharundharmaraj@gmail.com>
2023-09-26 15:01:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import Header from './Header'
-import './App.css';
+import Header from "./Header";
+import "./App.css";
+import Store from "./Store"; // Import the Store component
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counter"; // Import actions
 // import Store from './Store';
-
 function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <h1 className="counter-heading">Counter: {count}</h1>
+      <div className="button-container">
+        <button
+          className="increment-button"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          className="decrement-button"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+      <Store />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import counterReducer from "./counter"; // Import the counter reducer
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(counterReducer); // Create the Redux store with the counter reducer
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
